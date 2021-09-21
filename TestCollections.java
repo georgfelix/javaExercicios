410ce5eec0bf514b35cf602951bb553c2dbb65c1
Georg Felix <61664702+georgfelix@users.noreply.github.com>
2021-09-21 18:57:11 -0300

    mais

---
+++ TestCollections.java
@@ -0,0 +1,23 @@
+package Aulinhas;
+
+import java.util.ArrayList;
+
+public class TestCollections {
+
+	public static void main (String[] args) {
+		
+		ArrayList carrinho = new ArrayList();
+		Double valor = 100.55;
+		int valor2 = 1;
+		int valor3;
+		
+		carrinho.add(valor);
+		carrinho.add("Uva");
+		carrinho.add(valor2);
+		
+		System.out.println(carrinho.get(1));
+	}
+}
+
+
+
