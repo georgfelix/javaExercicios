f1b31b66d2f51fd458abc01901f4ae1b3a961c58
Georg Felix <61664702+georgfelix@users.noreply.github.com>
2021-10-07 20:06:56 -0300

    ajuste

--- ContaTelefone.java
+++ ContaTelefone.java
@@ -6,23 +6,23 @@ import java.util.Scanner;
 public class ContaTelefone {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Digite quantos minutos vc gastou: ");
-		
+
 		int minutos = sc.nextInt();
-		
+
 		double conta = 50.0;
-			
-		if (minutos >100) {
-			conta = conta +(minutos-100) * 2.0;
+
+		if (minutos > 100) {
+			conta += (minutos - 100) * 2.0; // é o mesmo que:
+//          conta = conta +(minutos-100) * 2.0;
 		}
 		System.out.printf("O valor da conta é: R$%.2f %n", conta);
-		
-					
+
 		sc.close();
 	}
 
