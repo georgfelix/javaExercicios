71f257be8a38e8131189acbb8a74ff1a709f3a3b
Georg Felix <61664702+georgfelix@users.noreply.github.com>
2022-01-03 20:44:29 -0300

    Add files via upload

---
+++ JavaProdutoPreco/src/entities/Product.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Product {
+	public String name;
+	public double price;
+	public int quantity;
+	
+//	O construtor abaixo faz com que se tenha que escrever um produto para nao haver produto nulo
+	
+	public Product (String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+//	Fim do construtor
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return name 
+				+ ", $ " + String.format("%.2f", price) 
+				+ ", " 
+				+ quantity + " units, Total: $ "
+				+ String.format("%.2f", totalValueInStock());
+	}
+}
+
